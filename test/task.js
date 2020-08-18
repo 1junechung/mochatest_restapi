badd90e5c4cca5dd90579f9241e5b50d32cd8586
june chung <1junechung@gmail.com>
2020-08-18 21:36:20 +0800

    first commit

--- test/task.js
+++ test/task.js
@@ -1,21 +1,20 @@
-let chai = require("chai");
-let chaiHttp = require("chai-http");
-let server = require("../index");
+let chai = require("chai"); // chai dependency
+let chaiHttp = require("chai-http"); // chai-http dependency for...
+let server = require("../index"); // actual app
 
-//Assertion Style
+//Assertion Style 
+
 chai.should();
-
 chai.use(chaiHttp);
 
 describe('Tasks API', () => {
-
     /**
-     * Test the GET route
+     * Test for GET 
      */
     describe("GET /api/tasks", () => {
         it("It should GET all the tasks", (done) => {
             chai.request(server)
-                .get("/api/tasks")
+                .get("/api/tasks") // this got into an error! without / in beginning
                 .end((err, response) => {
                     response.should.have.status(200);
                     response.body.should.be.a('array');
@@ -26,199 +25,119 @@ describe('Tasks API', () => {
 
         it("It should NOT GET all the tasks", (done) => {
             chai.request(server)
-                .get("/api/task")
+                .get("/api/taskwrong")
                 .end((err, response) => {
                     response.should.have.status(404);
                 done();
                 });
-        });
+        });        
 
     });
 
-
     /**
-     * Test the GET (by id) route
+     * Test for GET(by id)
      */
     describe("GET /api/tasks/:id", () => {
-        it("It should GET a task by ID", (done) => {
-            const taskId = 1;
-            chai.request(server)                
-                .get("/api/tasks/" + taskId)
+        it("It should GET task by ID", (done) => {
+            const taskId = 1; // define const out of actual test scope
+            chai.request(server)
+                .get("/api/tasks/" + taskId) // BE CAREFUL WHEN WRITING URI 
                 .end((err, response) => {
                     response.should.have.status(200);
                     response.body.should.be.a('object');
                     response.body.should.have.property('id');
                     response.body.should.have.property('name');
                     response.body.should.have.property('completed');
-                    response.body.should.have.property('id').eq(1);
+                    response.body.should.have.property('id').eq(1);   
                 done();
                 });
         });
 
-        it("It should NOT GET a task by ID", (done) => {
-            const taskId = 123;
-            chai.request(server)                
-                .get("/api/tasks/" + taskId)
+        it("It should NOT GET a tasks by ID", (done) => {
+            const taskId = 123; // weird task id
+            chai.request(server)
+                .get("/api/tasks/" + taskId) // BE CAREFUL WHEN WRITING URI 
                 .end((err, response) => {
                     response.should.have.status(404);
-                    response.text.should.be.eq("The task with the provided ID does not exist.");
+                    response.body.should.be.a('object');
+                    response.text.should.be.eql("The task with the provided ID does not exist.");
                 done();
                 });
-        });
-
+        });        
     });
-    
 
     /**
-     * Test the POST route
+     * Test for POST
      */
     describe("POST /api/tasks", () => {
         it("It should POST a new task", (done) => {
-            const task = {
+            // define a new task object
+            const task = {  
                 name: "Task 4",
                 completed: false
             };
-            chai.request(server)                
-                .post("/api/tasks")
-                .send(task)
+
+            chai.request(server)
+                .post("/api/tasks")  // make sure to change this to post 
+                .send(task) //need to send the object in POST
                 .end((err, response) => {
                     response.should.have.status(201);
                     response.body.should.be.a('object');
+                    response.body.should.have.property('id');
+                    response.body.should.have.property('name');
+                    response.body.should.have.property('completed');
                     response.body.should.have.property('id').eq(4);
                     response.body.should.have.property('name').eq("Task 4");
-                    response.body.should.have.property('completed').eq(false);
+                    response.body.should.have.property('completed').eq(false);                       
                 done();
                 });
         });
 
-        it("It should NOT POST a new task without the name property", (done) => {
-            const task = {
+        it("It should NOT POST a new task", (done) => {
+            // define a new task object
+            const task = {  
+                name: "Ta", // less than 3 characters(task-schema check)
                 completed: false
             };
-            chai.request(server)                
-                .post("/api/tasks")
-                .send(task)
+
+            chai.request(server)
+                .post("/api/tasks")  // make sure to change this to post 
+                .send(task) //need to send the object in POST
                 .end((err, response) => {
                     response.should.have.status(400);
-                    response.text.should.be.eq("The name should be at least 3 chars long!");
+                    response.text.should.be.eql("The name should be at least 3 chars long!");               
                 done();
                 });
         });
-
-    });
-
-
+    });    
     /**
-     * Test the PUT route
-     */
-    describe("PUT /api/tasks/:id", () => {
-        it("It should PUT an existing task", (done) => {
-            const taskId = 1;
-            const task = {
-                name: "Task 1 changed",
-                completed: true
-            };
-            chai.request(server)                
-                .put("/api/tasks/" + taskId)
-                .send(task)
-                .end((err, response) => {
-                    response.should.have.status(200);
-                    response.body.should.be.a('object');
-                    response.body.should.have.property('id').eq(1);
-                    response.body.should.have.property('name').eq("Task 1 changed");
-                    response.body.should.have.property('completed').eq(true);
-                done();
-                });
-        });
-
-        it("It should NOT PUT an existing task with a name with less than 3 characters", (done) => {
-            const taskId = 1;
-            const task = {
-                name: "Ta",
-                completed: true
-            };
-            chai.request(server)                
-                .put("/api/tasks/" + taskId)
-                .send(task)
-                .end((err, response) => {
-                    response.should.have.status(400);
-                    response.text.should.be.eq("The name should be at least 3 chars long!");
-                done();
-                });
-        });        
-    });
-    
-
-    /**
-     * Test the PATCH route
+     * Test for PUT 
      */
 
-    describe("PATCH /api/tasks/:id", () => {
-        it("It should PATCH an existing task", (done) => {
-            const taskId = 1;
-            const task = {
-                name: "Task 1 patch"
-            };
-            chai.request(server)                
-                .patch("/api/tasks/" + taskId)
-                .send(task)
-                .end((err, response) => {
-                    response.should.have.status(200);
-                    response.body.should.be.a('object');
-                    response.body.should.have.property('id').eq(1);
-                    response.body.should.have.property('name').eq("Task 1 patch");
-                    response.body.should.have.property('completed').eq(true);
-                done();
-                });
-        });
-
-        it("It should NOT PATCH an existing task with a name with less than 3 characters", (done) => {
-            const taskId = 1;
-            const task = {
-                name: "Ta"
-            };
-            chai.request(server)                
-                .patch("/api/tasks/" + taskId)
-                .send(task)
-                .end((err, response) => {
-                    response.should.have.status(400);
-                    response.text.should.be.eq("The name should be at least 3 chars long!");
-                done();
-                });
-        });        
-    });
-    
-
     /**
-     * Test the DELETE route
+     * Test for DELETE 
      */
     describe("DELETE /api/tasks/:id", () => {
-        it("It should DELETE an existing task", (done) => {
+        it("It should DELETE a task by id", (done) => {
             const taskId = 1;
-            chai.request(server)                
-                .delete("/api/tasks/" + taskId)
+            chai.request(server)
+                .delete("/api/tasks/" + taskId)  
                 .end((err, response) => {
-                    response.should.have.status(200);
+                    response.should.have.status(200);                    
                 done();
                 });
         });
 
-        it("It should NOT DELETE a task that is not in the database", (done) => {
+        it("It should NOT DELETE a task by id", (done) => {
             const taskId = 145;
-            chai.request(server)                
-                .delete("/api/tasks/" + taskId)
+            chai.request(server)
+                .delete("/api/tasks/" + taskId)  
                 .end((err, response) => {
-                    response.should.have.status(404);
-                    response.text.should.be.eq("The task with the provided ID does not exist.");
+                    response.should.have.status(404); 
+                    response.text.should.be.eql("The task with the provided ID does not exist.");                
                 done();
                 });
         });
-
     });
-
-
-
-
 });
-
 
